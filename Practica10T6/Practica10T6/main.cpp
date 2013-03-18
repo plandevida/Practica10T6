313d3fc96ff37ee075e156e22837de875c2df3fd
plandevida <plandevida@planes.com>
2013-03-18 15:18:12 +0100

    proyecto de xcode (MacOsX)

---
+++ Practica10T6/Practica10T6/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Practica10T6
+//
+//  Created by Dani on 18/03/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
